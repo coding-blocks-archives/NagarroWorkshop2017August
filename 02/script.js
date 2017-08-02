eb34e97dbc261e8346a06cb39d677318285e961e
Arnav Gupta <deploy-bot@codingblocks.com>
2017-08-02 14:34:00 +0530

    part 1 and 2
    Signed-off-by: Arnav Gupta <deploy-bot@codingblocks.com>

---
+++ 02/script.js
@@ -0,0 +1,6 @@
+// console.error('this is an error')
+// console.warn('this is a warning')
+// console.info('this is an info')
+// console.log('this is just a log')
+var mt = document.getElementById('mytext')
+mt.innerHTML = 'some other stuff'
